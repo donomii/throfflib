974bd7aa728409dea15b54d37a5ac95874738af7
Jeremy Price <donomii@gmail.com>
2015-09-12 02:11:07 +0200

    Added wg gtaphics library

--- throffgraphics.go
+++ throffgraphics.go
@@ -6,6 +6,8 @@ package throfflib
 import (    
 
  "strconv"
+"github.com/magsoft-se/wg"
+"image/color"
  "github.com/go-gl/glfw/v3.1/glfw"
  "github.com/go-gl/gl/v2.1/gl"
  	"image"
@@ -14,6 +16,11 @@ import (
 	"os"
 	"runtime"
 )
+type Point struct {
+        x int
+        y int
+}
+
 
 var (
 	texture   uint32
@@ -28,41 +35,100 @@ var (
     runtime.LockOSThread()
 }
 
+var CallbackState *Engine
+
+func WgCallback () {
+
+CallbackState = CallbackState.RunString("WGCALLBACK", "wg callback")
+
+}
 		
 		
 //Creates a new engine and populates it with the core functions
 func LoadGraphics(e *Engine) *Engine{
 	
 	
-		e=add(e, "glfw.Init", NewCode("glfw.Init", 0, func (e *Engine,c *Thingy) *Engine {
-			    err := glfw.Init()
-				if err != nil {
-					panic(err)
-				}
-		return e}))
-		
-		e=add(e, "GLFWEVENTLOOP", NewCode("GLFWEVENTLOOP", 1, func (e *Engine,c *Thingy) *Engine {
-			var el1 *Thingy
-			el1, e.dataStack = popStack(e.dataStack)
-			window := el1._structVal.(*glfw.Window)
-			window.MakeContextCurrent()
+	e=add(e, "glfw.Init", NewCode("glfw.Init", 0, func (e *Engine,c *Thingy) *Engine {
+		    err := glfw.Init()
+			if err != nil {
+				panic(err)
+			}
+	return e}))
+	
+	e=add(e, "GLFWEVENTLOOP", NewCode("GLFWEVENTLOOP", 1, func (e *Engine,c *Thingy) *Engine {
+		var el1 *Thingy
+		el1, e.dataStack = popStack(e.dataStack)
+		window := el1._structVal.(*glfw.Window)
+		window.MakeContextCurrent()
 
-    	if err := gl.Init(); err != nil {
-		panic(err)
-	}
+		if err := gl.Init(); err != nil {
+			panic(err)
+		}
 
-	texture = newTexture("square.png")
-	defer gl.DeleteTextures(1, &texture)
+		texture = newTexture("square.png")
+		defer gl.DeleteTextures(1, &texture)
 
-	setupScene()
-	for !window.ShouldClose() {
-		drawScene()
-		window.SwapBuffers()
-		glfw.PollEvents()
-	}
+		setupScene()
+		for !window.ShouldClose() {
+			drawScene()
+			window.SwapBuffers()
+			glfw.PollEvents()
+		}
 	
 
 		return e}))
+
+
+	e=add(e, "WG.CLEAR", NewCode("WG.CLEAR", 1, func (e *Engine,c *Thingy) *Engine {
+		var el3 *Thingy
+		el3, e.dataStack = popStack(e.dataStack)
+		col := el3._structVal.(color.RGBA)
+		wg.ClearImage(col)
+		return e}))
+
+	e=add(e, "WG.SETPOINT", NewCode("WG.SETPOINT", 3, func (e *Engine,c *Thingy) *Engine {
+		var el1, el2,el3 *Thingy
+		el1, e.dataStack = popStack(e.dataStack)
+		el2, e.dataStack = popStack(e.dataStack)
+		el3, e.dataStack = popStack(e.dataStack)
+		x1, _ := strconv.ParseInt( el1.getString(), 10, 8 )
+		x2, _ := strconv.ParseInt( el2.getString(), 10, 8 )
+		col := el3._structVal.(color.RGBA)
+		wg.GetImage().Set(int(x1),int(x2),col)
+		return e}))
+
+
+	e=add(e, "RGBA", NewCode("RGBA", 3, func (e *Engine,c *Thingy) *Engine {
+		var el1, el2,el3,el4 *Thingy
+		el1, e.dataStack = popStack(e.dataStack)
+		el2, e.dataStack = popStack(e.dataStack)
+		el3, e.dataStack = popStack(e.dataStack)
+		el4, e.dataStack = popStack(e.dataStack)
+		x1, _ := strconv.ParseInt( el1.getString(), 10, 32 )
+		x2, _ := strconv.ParseInt( el2.getString(), 10, 32 )
+		x3, _ := strconv.ParseInt( el3.getString(), 10, 32 )
+		x4, _ := strconv.ParseInt( el4.getString(), 10, 32 )
+		col := color.RGBA{uint8(x1),uint8(x2),uint8(x3),uint8(x4)}
+		e.dataStack = append(e.dataStack, NewWrapper(col))
+		return e}))
+	e=add(e, "WG.GETKEY", NewCode("WG.GETKEY", 0, func (e *Engine,c *Thingy) *Engine {
+		var el1 *Thingy
+		el1, e.dataStack = popStack(e.dataStack)
+		xx, _ := strconv.ParseInt( el1.getString(), 10, 32 )
+		var ret *Thingy
+		if wg.GetKey(int(xx)) {
+			ret = NewBool(1)
+		} else {
+			ret = NewBool(0)
+		}	
+		e.dataStack = append(e.dataStack, ret)
+		return e}))
+ 	e=add(e, "WG.START", NewCode("WG.START", 1, func (e *Engine,c *Thingy) *Engine {
+		CallbackState = e
+		wg.Start(800, 600, 8500, WgCallback)
+		return e}))
+
+
 		
 	e=add(e, "glfw.CreateWindow",  NewCode("glfw.CreateWindow", 2, func (ne *Engine,c *Thingy) *Engine {
 		var x,y,title *Thingy
@@ -76,14 +142,14 @@ func LoadGraphics(e *Engine) *Engine{
 		
 		
 		glfw.WindowHint(glfw.Resizable, glfw.False)
-	glfw.WindowHint(glfw.ContextVersionMajor, 2)
-	glfw.WindowHint(glfw.ContextVersionMinor, 1)
-		
-		    window, err := glfw.CreateWindow(int(xx), int(yy), title.getString(), nil, nil)
-    if err != nil {
-        panic(err)
-    }
-		
+		glfw.WindowHint(glfw.ContextVersionMajor, 2)
+		glfw.WindowHint(glfw.ContextVersionMinor, 1)
+			
+		window, err := glfw.CreateWindow(int(xx), int(yy), title.getString(), nil, nil)
+		if err != nil {
+			panic(err)
+		}
+			
 		ne.dataStack = pushStack(ne.dataStack, NewWrapper(window))
 		return ne}))
 return e	
